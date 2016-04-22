f082dfe259c83b314a74c1815d0bce2a53cb0149
Rob Looby <robertjlooby@gmail.com>
2016-04-22 15:54:16 -0500

    Wire up StartApp

--- Main.elm
+++ Main.elm
@@ -2,8 +2,11 @@ module Main (..) where
 
 import Box
 import BoxList
+import Effects exposing (Effects, Never)
 import Html exposing (div, Html, text)
 import Html.Attributes exposing (style)
+import StartApp exposing (start)
+import Task
 
 
 type alias Model =
@@ -11,15 +14,37 @@ type alias Model =
   }
 
 
-main : Html
-main =
-  view
-    <| Model
+type Action
+  = None
+
+
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+  ( model, Effects.none )
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div [] <| List.map BoxList.view model.boxLists
+
+
+app : StartApp.App Model
+app =
+  let
+    initialModel =
+      Model
         [ BoxList.Model [ Box.Model, Box.Model, Box.Model ]
         , BoxList.Model [ Box.Model, Box.Model ]
         ]
+  in
+    start { init = ( initialModel, Effects.none ), view = view, update = update, inputs = [] }
 
 
-view : Model -> Html
-view model =
-  div [] <| List.map BoxList.view model.boxLists
+main : Signal Html
+main =
+  app.html
+
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
