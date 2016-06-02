f74e6fcd2b99ec06536d95e29e3477c990f3c778
Rob Looby <robertjlooby@gmail.com>
2016-06-02 13:44:12 -0500

    Upgrade to 0.17

--- BoxList.elm
+++ BoxList.elm
@@ -1,82 +1,80 @@
-module BoxList (..) where
+module BoxList exposing (..)
 
 import Box
 import Color exposing (Color)
 import Html exposing (button, div, Html, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
-import Random.PCG as Random exposing (generate, Generator)
+import Random.Pcg as Random exposing (step, Generator)
 import Uuid exposing (Uuid, uuidGenerator)
 
 
 type alias Model =
-  { boxes : List Box.Model
-  , seed : Random.Seed
-  }
+    { boxes : List Box.Model
+    , seed : Random.Seed
+    }
 
 
-type Action
-  = AddBox
+type Msg
+    = AddBox
 
 
 boxGenerator : Generator Box.Model
 boxGenerator =
-  let
-    intGenerator =
-      Random.int 0 255
+    let
+        intGenerator =
+            Random.int 0 255
 
-    colorGenerator =
-      Random.map3 Color.rgb intGenerator intGenerator intGenerator
-  in
-    Random.map2 Box.Model colorGenerator uuidGenerator
+        colorGenerator =
+            Random.map3 Color.rgb intGenerator intGenerator intGenerator
+    in
+        Random.map2 Box.Model colorGenerator uuidGenerator
 
 
-update : Action -> Model -> Model
-update action model =
-  case action of
-    AddBox ->
-      let
-        ( newBox, newSeed ) =
-          generate boxGenerator model.seed
-      in
-        { model
-          | boxes = List.append model.boxes [ newBox ]
-          , seed = newSeed
-        }
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddBox ->
+            let
+                ( newBox, newSeed ) =
+                    step boxGenerator model.seed
+            in
+                { model
+                    | boxes = List.append model.boxes [ newBox ]
+                    , seed = newSeed
+                }
 
 
 removeBox : Uuid -> Model -> ( Maybe Box.Model, Model )
 removeBox id model =
-  let
-    ( removedBox, listWithoutRemovedBox ) =
-      List.foldr
-        (\box ( maybeBox, list ) ->
-          if box.id == id then
-            ( Just box, list )
-          else
-            ( maybeBox, box :: list )
-        )
-        ( Nothing, [] )
-        model.boxes
-  in
-    ( removedBox, { model | boxes = listWithoutRemovedBox } )
+    let
+        ( removedBox, listWithoutRemovedBox ) =
+            List.foldr
+                (\box ( maybeBox, list ) ->
+                    if box.id == id then
+                        ( Just box, list )
+                    else
+                        ( maybeBox, box :: list )
+                )
+                ( Nothing, [] )
+                model.boxes
+    in
+        ( removedBox, { model | boxes = listWithoutRemovedBox } )
 
 
-view : Signal.Address Action -> Model -> Html
-view address model =
-  let
-    boxesStyle =
-      style
-        [ ( "border", "black solid" )
-        , ( "overflow", "auto" )
-        , ( "width", "110px" )
-        , ( "height", "100%" )
-        , ( "margin", "5px" )
-        , ( "min-height", "100px" )
-        ]
-  in
-    div
-      [ boxesStyle ]
-      <| List.append
-          [ button [ onClick address AddBox ] [ text "+" ] ]
-          (List.map Box.view model.boxes)
+view : Model -> Html Msg
+view model =
+    let
+        boxesStyle =
+            style
+                [ ( "border", "black solid" )
+                , ( "overflow", "auto" )
+                , ( "width", "110px" )
+                , ( "height", "100%" )
+                , ( "margin", "5px" )
+                , ( "min-height", "100px" )
+                ]
+    in
+        div [ boxesStyle ]
+            <| List.append [ button [ onClick AddBox ] [ text "+" ] ]
+                (List.map Box.view model.boxes)
--- Main.elm
+++ Main.elm
@@ -1,183 +1,163 @@
-module Main (..) where
+module Main exposing (..)
 
 import Box
 import BoxList
-import Effects exposing (Effects, Never)
 import Html exposing (button, div, Html, text)
+import Html.App as App
 import Html.Attributes exposing (style)
-import Html.Events exposing (on, onClick, onWithOptions, targetValue)
-import Json.Decode exposing (at, customDecoder, Decoder, int, string, succeed)
-import Random.PCG as Random exposing (generate, Generator, independentSeed, initialSeed)
-import StartApp exposing (start)
-import String
-import Task
+import Html.Events exposing (on, onClick, onWithOptions)
+import Json.Decode as Decode exposing (at, customDecoder, Decoder, int, string, succeed)
+import Random.Pcg as Random exposing (step, Generator, independentSeed, initialSeed)
 import Uuid exposing (Uuid)
 
 
 type alias ID =
-  Int
+    Int
 
 
 type alias Model =
-  { boxLists : List ( ID, BoxList.Model )
-  , nextId : ID
-  , lastDragged : Maybe Uuid
-  , seed : Random.Seed
-  }
+    { boxLists : List ( ID, BoxList.Model )
+    , nextId : ID
+    , lastDragged : Maybe Uuid
+    , seed : Random.Seed
+    }
 
 
-type Action
-  = AddNewBoxList
-  | RemoveBoxList ID
-  | ModifyBoxList ID BoxList.Action
-  | UpdateLastDragged Uuid
-  | HandleDropped ID
-  | NoOp
+type Msg
+    = AddNewBoxList
+    | RemoveBoxList ID
+    | ModifyBoxList ID BoxList.Msg
+    | UpdateLastDragged Uuid
+    | HandleDropped ID
+    | NoOp
 
 
 boxListGenerator : Generator BoxList.Model
 boxListGenerator =
-  Random.map (BoxList.Model []) independentSeed
+    Random.map (BoxList.Model []) independentSeed
 
 
-update : Action -> Model -> ( Model, Effects Action )
-update action model =
-  case action of
-    NoOp ->
-      ( model, Effects.none )
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
 
-    AddNewBoxList ->
-      let
-        ( newBoxList, newSeed ) =
-          generate boxListGenerator model.seed
+        AddNewBoxList ->
+            let
+                ( newBoxList, newSeed ) =
+                    step boxListGenerator model.seed
 
-        newModel =
-          { model
-            | boxLists = List.append model.boxLists [ ( model.nextId, newBoxList ) ]
-            , nextId = model.nextId + 1
-            , seed = newSeed
-          }
-      in
-        ( newModel, Effects.none )
+                newModel =
+                    { model
+                        | boxLists = List.append model.boxLists [ ( model.nextId, newBoxList ) ]
+                        , nextId = model.nextId + 1
+                        , seed = newSeed
+                    }
+            in
+                ( newModel, Cmd.none )
 
-    RemoveBoxList id ->
-      let
-        newModel =
-          { model
-            | boxLists = List.filter (\( id', _ ) -> id' /= id) model.boxLists
-          }
-      in
-        ( newModel, Effects.none )
+        RemoveBoxList id ->
+            let
+                newModel =
+                    { model
+                        | boxLists = List.filter (\( id', _ ) -> id' /= id) model.boxLists
+                    }
+            in
+                ( newModel, Cmd.none )
 
-    ModifyBoxList id boxListAction ->
-      let
-        updateById ( id', boxList ) =
-          if id' == id then
-            ( id', BoxList.update boxListAction boxList )
-          else
-            ( id', boxList )
+        ModifyBoxList id boxListAction ->
+            let
+                updateById ( id', boxList ) =
+                    if id' == id then
+                        ( id', BoxList.update boxListAction boxList )
+                    else
+                        ( id', boxList )
 
-        newModel =
-          { model | boxLists = List.map updateById model.boxLists }
-      in
-        ( newModel, Effects.none )
+                newModel =
+                    { model | boxLists = List.map updateById model.boxLists }
+            in
+                ( newModel, Cmd.none )
 
-    UpdateLastDragged boxId ->
-      ( { model | lastDragged = Just boxId }, Effects.none )
+        UpdateLastDragged boxId ->
+            ( { model | lastDragged = Just boxId }, Cmd.none )
 
-    HandleDropped droppedBoxListId ->
-      case model.lastDragged of
-        Nothing ->
-          ( model, Effects.none )
+        HandleDropped droppedBoxListId ->
+            case model.lastDragged of
+                Nothing ->
+                    ( model, Cmd.none )
 
-        Just boxId ->
-          let
-            partitionedLists =
-              List.map (\( id, boxList ) -> ( id, BoxList.removeBox boxId boxList )) model.boxLists
+                Just boxId ->
+                    let
+                        partitionedLists =
+                            List.map (\( id, boxList ) -> ( id, BoxList.removeBox boxId boxList )) model.boxLists
 
-            draggedBox =
-              List.filterMap (\( _, ( draggedBox, _ ) ) -> draggedBox) partitionedLists
-                |> List.head
-          in
-            case draggedBox of
-              Nothing ->
-                ( model, Effects.none )
+                        draggedBox =
+                            List.filterMap (\( _, ( draggedBox, _ ) ) -> draggedBox) partitionedLists
+                                |> List.head
+                    in
+                        case draggedBox of
+                            Nothing ->
+                                ( model, Cmd.none )
 
-              Just box ->
-                let
-                  newBoxLists =
-                    List.foldr
-                      (\( id, ( _, boxList ) ) boxLists ->
-                        if id == droppedBoxListId then
-                          ( id, { boxList | boxes = List.append boxList.boxes [ box ] } ) :: boxLists
-                        else
-                          ( id, boxList ) :: boxLists
-                      )
-                      []
-                      partitionedLists
+                            Just box ->
+                                let
+                                    newBoxLists =
+                                        List.foldr
+                                            (\( id, ( _, boxList ) ) boxLists ->
+                                                if id == droppedBoxListId then
+                                                    ( id, { boxList | boxes = List.append boxList.boxes [ box ] } ) :: boxLists
+                                                else
+                                                    ( id, boxList ) :: boxLists
+                                            )
+                                            []
+                                            partitionedLists
 
-                  newModel =
-                    { model | boxLists = newBoxLists }
-                in
-                  ( newModel, Effects.none )
+                                    newModel =
+                                        { model | boxLists = newBoxLists }
+                                in
+                                    ( newModel, Cmd.none )
 
 
-view : Signal.Address Action -> Model -> Html
-view address model =
-  div
-    []
-    [ div [] <| List.map (boxListView address) model.boxLists
-    , button [ onClick address AddNewBoxList ] [ text "Add new list" ]
-    ]
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] <| List.map boxListView model.boxLists
+        , button [ onClick AddNewBoxList ] [ text "Add new list" ]
+        ]
 
 
 uuidDecoder : Decoder Uuid
 uuidDecoder =
-  customDecoder string (Result.fromMaybe "failed to parse Uuid" << Uuid.fromString)
+    customDecoder string (Result.fromMaybe "failed to parse Uuid" << Uuid.fromString)
 
 
-boxListView : Signal.Address Action -> ( ID, BoxList.Model ) -> Html
-boxListView address ( id, boxList ) =
-  div
-    [ style [ ( "float", "left" ) ]
-    , on
-        "dragstart"
-        (at [ "target", "id" ] uuidDecoder)
-        (\boxId -> Signal.message address <| UpdateLastDragged boxId)
-    , on
-        "drop"
-        (succeed "dropped")
-        (\_ -> Signal.message address (HandleDropped id))
-    , onWithOptions
-        "dragover"
-        { preventDefault = True, stopPropagation = True }
-        (succeed "dragged over")
-        (\_ -> Signal.message address NoOp)
-    ]
-    [ (BoxList.view (Signal.forwardTo address <| ModifyBoxList id) boxList)
-    , button [ onClick address <| RemoveBoxList id ] [ text "Remove" ]
-    ]
+boxListView : ( ID, BoxList.Model ) -> Html Msg
+boxListView ( id, boxList ) =
+    div
+        [ style [ ( "float", "left" ) ]
+        , on "dragstart"
+            (Decode.map UpdateLastDragged (at [ "target", "id" ] uuidDecoder))
+        , on "drop"
+            (succeed <| HandleDropped id)
+        , onWithOptions "dragover"
+            { preventDefault = True, stopPropagation = True }
+            (succeed NoOp)
+        ]
+        [ App.map (ModifyBoxList id) (BoxList.view boxList)
+        , button [ onClick <| RemoveBoxList id ] [ text "Remove" ]
+        ]
 
 
-app : StartApp.App Model
-app =
-  let
-    initialModel =
-      Model [] 0 Nothing (initialSeed 0)
-  in
-    start
-      { init = ( initialModel, Effects.none )
-      , view = view
-      , update = update
-      , inputs = []
-      }
-
-
-main : Signal Html
+main : Program Never
 main =
-  app.html
-
-
-port tasks : Signal (Task.Task Never ())
-port tasks =
-  app.tasks
+    let
+        initialModel =
+            Model [] 0 Nothing (initialSeed 0)
+    in
+        App.program
+            { init = ( initialModel, Cmd.none )
+            , view = view
+            , update = update
+            , subscriptions = (\_ -> Sub.none)
+            }
